345b4aa41b797bcf92fa76bd899dd45a0bae5e92
Kavin1421 <kkavinkumar24@gmail.com>
2023-05-16 21:13:18 +0530

    Commerece

--- Kavin_Project/settings.py
+++ Kavin_Project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1jl(#0s@4^7u^*r)du^@d0%@9*&@axfhwtb3q#=2bwqoah4y7v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
