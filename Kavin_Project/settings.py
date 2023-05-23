44c7d163c01abf2f9f990c9ff1e9b4a591dc854b
Kavinkumar K <109059452+Kavin1421@users.noreply.github.com>
2023-05-23 21:20:56 +0530

    settings.py

--- Kavin_Project/settings.py
+++ Kavin_Project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangokavin',
         'HOST': 'localhost',
-        'USER': 'root',
+        'USER': 'admin',
         'PASSWORD': '1421@Kavin',
         'PORT': '3306',
     }
